6e086b004d14d1bb3de380e023514e841ce0a970
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2025-11-15 20:25:05 +0000

    trial

--- riscv/assembly/src/div.s
+++ riscv/assembly/src/div.s
@@ -20,32 +20,47 @@ div:
     # we further let loop counter be t2 
     beq  a1, x0, .div_by_zero   # if denominator == 0
     
+    # intitial initialisation of Q and R 
     li t0, 0 # Q, alternatively, addi t0, x0, 0
     li t1, 0 # R
-    li t2, 31 
+    # li t2, 31 # initialisation of counter
+
+    mv t2, a0
+
 .loop: 
+    # i < 0
     blt t2, zero, .exit  
-    
+
+    # bit shift R to the left by 1 
     slli t1, t1, 1 
     
+    # extract the ith bit from N, and put it in the 0th index  
     srl t5, a0, t2
     andi t5, t5, 1  
     
+    # assign the ith bit of N to the LSB of R 
     or t1, t1, t5  
     
+    # if D < R, we skip the subtraction stages, and decrement the counter
     bltu t1, a1, .dec
     
+    # subtract D from R 
     sub t1, t1, a1 
     
+    # create a value with register i being of value 1, and 0s otherwise. Then, assign it to Q (a0)
     li t3, 1 
     sll t3, t3, t2
     or t0, t0, t3
 
 .dec: 
-    addi t2, t2, -1
+
+    #decrement counter
+    #addi t2, t2, -1
+    srli t2, t2, 1
     j .loop
     
-.exit: 
+.exit:
+    # store the values in the appropriate registers, then return. 
     mv a0, t0 
     mv a1, t1 
     # DEBUG_PRINT a0
@@ -59,6 +74,8 @@ div:
     ret
 
 .div_by_zero:
+
+    # edge case that numerator is 0. 
     li   a0, 0
     li   a1, 0
     lw   ra, 28(sp)
